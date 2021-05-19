277996f11fa875156245b7814cfb1defbe87aa2d
HraausLiak <ren41999450647@163.com>
2021-05-19 17:44:11 +0800

    Add        .gitignore Add        CMakeLists.txt Add        public/CMakeLists.txt Add        public/base/application.cc Add        public/base/application.h Add        public/base/exception.h Add        public/base/option.cc Add        public/base/option.h Add        public/base/option_set.cc Add        public/base/option_set.h Add        realmd/CMakeLists.txt Add        realmd/application.cc Add        realmd/application.h Add        realmd/main.cc

---
+++ public/base/application.h
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * Copyright (C) 2015  671643387@qq.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *****************************************************************************/
+
+#ifndef PUBLIC_BASE_APPLICATION_H_
+#define PUBLIC_BASE_APPLICATION_H_
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif
+
+#include <string>
+#include <vector>
+
+#include "public/base/option.h"
+#include "public/base/option_set.h"
+
+namespace pear {
+    namespace base {
+
+        class Application
+        {
+        public:
+            enum ExitCode
+            {
+                EXIT_OK = 0,
+                EXIT_USAGE = 64,
+                EXIT_CONFIG = 65,
+                EXIT_SOFTWARE = 70,
+            };
+
+            Application(void);
+            virtual ~Application(void);
+
+            int Run(int argc, char* argv[]);
+
+            void HookSignal(int signal);
+            void UnhookSignal(int signal);
+
+        protected:
+            virtual void OnDefineOptions(OptionSet& options) = 0;
+            virtual int OnOption(const ::std::string& name, const ::std::string& arg) = 0;
+            virtual void OnSignal(int signal);
+
+            virtual int OnInitialize(void) = 0;
+            virtual void OnUninitialize(void) = 0;
+
+            virtual int Main(::std::vector<::std::string>& unknownArgs) = 0;
+
+        private:
+            int Initialize(int argc, char* argv[]);
+            void Uninitialize(void);
+
+            static void SignalHandler(int signal);
+
+
+            ::std::vector<::std::string> arguemnts_;
+            OptionSet options_;
+
+            static Application *app_;
+        };
+
+    }
+}
+
+#endif // PUBLIC_BASE_APPLICATION_H_
